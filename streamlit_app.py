a4990fed8a70b9881db677d2c97cacc71d1f195a
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2023-10-22 18:39:51 -0300

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -85,7 +85,7 @@ with st.container():
             f'</div>',
             unsafe_allow_html=True
         )
-        st.markdown("[Link](https://uberviajes.streamlit.app/)", unsafe_allow_html=True)        
+        st.markdown("<h1 style='text-align: center;'><a href='https://uberviajes.streamlit.app/'>Link</a></h1>", unsafe_allow_html=True)        
     with col51:
         st.markdown("<h3 style='text-align:center; color: white'>Study Case Coursera</h3>", unsafe_allow_html=True)
         st.image(cyclist, width=300)
