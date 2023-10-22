47f56a8e8bea10b354be8ea97c79947ead38a419
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2023-10-22 16:12:45 -0300

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,7 @@
 import streamlit as st
+import requests
+from PIL import Image
 import base64
-
-
-@st.cache_data
-def get_img_as_base64(file):
-    with open(file, "rb") as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
-
-
-img = get_img_as_base64("image.jpg")
 
 page_bg_img = f"""
 <style>
