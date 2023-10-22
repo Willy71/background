9070bf9e4fa3f4861977fe0926254059380efab6
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2023-10-22 15:34:10 -0300

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,5 +32,5 @@ right: 2rem;
 """
 
 st.markdown(page_bg_img, unsafe_allow_html=True)
-st.markdown("<h1 style='text-align: center'>Guillermo Cerato</h1>", unsafe_allow_html=True)
-st.markdown("<h2 style='text-align: center'>Data analyst</h2>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: color: white; center'>Guillermo Cerato</h1>", unsafe_allow_html=True)
+st.markdown("<h2 style='text-align: color: white;center'>Data analyst</h2>", unsafe_allow_html=True)
