8debcb00c1f469740f15e08980f0e34f515f1d40
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2023-10-22 17:53:32 -0300

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,7 @@ st.markdown(page_bg_img, unsafe_allow_html=True)
 
 st.markdown(
     f'<div style="display: flex; justify-content: center;">'
-    f'<img src="https://i.postimg.cc/BnNQFd7x/nombre.png" width="600">'
+    f'<img src="https://i.postimg.cc/BnNQFd7x/nombre.png" width="800">'
     f'</div>',
     unsafe_allow_html=True
 )
