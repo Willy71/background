ed3a012dc45510bd67d356e27086e4d8d0772694
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2023-10-26 20:37:30 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import requests
 from PIL import Image
 import base64
+from state import count_sessions
 
 # Colocar nome na pagina, icone e ampliar a tela
 st.set_page_config(
@@ -141,5 +142,7 @@ with st.container():
             )              
 st.write("---")
 
-st.markdown(<h5 style='text-align: center;'><a href='http://www.freevisitorcounters.com'>Free Visitor Counters</a> <script type='text/javascript' src='https://www.freevisitorcounters.com/auth.php?id=c2ecb4df5f3e5f30e6623e0ccb2c164c450b6043'></script>
-<script type="text/javascript" src="https://www.freevisitorcounters.com/en/home/counter/1092916/t/11"></script></a></h5>, unsafe_allow_html=True)
+count_sessions()
+
+st.write("test")
+
