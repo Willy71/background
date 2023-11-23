cef3e06668900c2a2e75a865559cdeab2cf0ba12
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2023-11-23 08:26:25 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import requests
 from PIL import Image
 import base64
 
+st.write("#")
+
 # Colocar nome na pagina, icone e ampliar a tela
 st.set_page_config(
     page_title="Portfolio - Guillermo Cerato",
@@ -43,8 +45,6 @@ right: 2rem;
 </style>
 """
 st.markdown(page_bg_img, unsafe_allow_html=True)
-
-st.write("#")
 
 st.markdown(
     f'<div style="display: flex; justify-content: center;">'
