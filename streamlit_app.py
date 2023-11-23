65d1ef3066c8a2ae7cd989026efb81e7606b7082
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2023-11-23 18:03:15 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -195,24 +195,18 @@ centrar_texto("Excellent person and excellent professional. Very human and ideal
 st.markdown("<h1 style='text-align: center; color: white'>Contact</h1>", unsafe_allow_html=True)
 
 with st.container():    
-    col50, col51, col52, col53, col54 = st.columns(5)
+    col50, col51, col52, col53, col54, col55, col56, col57 = st.columns(8)
     with col51:
         photo_link("Kaggle", "https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/189_Kaggle_logo_logos-512.png", "https://www.kaggle.com/willycerato", "50px")
     with col52:
         photo_link("Github", "https://cdn.pixabay.com/photo/2022/01/30/13/33/github-6980894_1280.png", "https://github.com/Willy71", "50px")
     with col53:
         photo_link("Instagram", "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/150px-Instagram_logo_2022.svg.png", "https://www.instagram.com/willycerato", "50px")
-            
-st.title("")
-st.title("")
-
-with st.container():    
-    col55, col56, col57, col58, col59 = st.columns(5)
-    with col56:
+    with col54:
         photo_link("Facebook", "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/2023_Facebook_icon.svg/50px-2023_Facebook_icon.svg.png", "https://www.facebook.com/guillermo.cerato", "50px")
-    with col57:
+    with col55:
         photo_link("Linkedin", "https://img.freepik.com/vetores-premium/logotipo-quadrado-do-linkedin-isolado-no-fundo-branco_469489-892.jpg", "https://www.linkedin.com/in/willycerato", "50px")        
-    with col58:
+    with col56:
         photo_link("Whatsapp", "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/120px-WhatsApp.svg.png", "https://wa.me/5542991657847", "50px")
 
     
