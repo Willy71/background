fe87e2b6d4aada6b73ade9cb9f7d1af26ebcfa97
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2023-11-23 17:59:59 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,6 +44,10 @@ right: 2rem;
 """
 st.markdown(page_bg_img, unsafe_allow_html=True)
 
+def photo_link(alt_text, img_url, link_url, img_width):
+    markdown_code = f'<a href="{link_url}" target="_blank"><img src="{img_url}" alt="{alt_text}" width="{img_width}"></a>'
+    st.markdown(markdown_code, unsafe_allow_html=True)
+
 st.write("#")
 
 st.markdown(
@@ -140,19 +144,6 @@ with st.container():
 st.markdown("")
 st.markdown("")
 
-with st.container():    
-    col60, col61, col62, col63, col64, col65 = st.columns(6)
-    with col61:
-        centrar_texto("[Kaggle](https://www.kaggle.com/willycerato)", 7, "blue")
-    with col62:
-        centrar_texto("[Github](https://github.com/Willy71)", 7, "blue")
-    with col63:
-        centrar_texto("[Instagram](https://www.instagram.com/willycerato)", 7, "blue")
-    with col64:
-        centrar_texto("[Facebook](https://www.facebook.com/guillermo.cerato)", 7, "blue")
-
-st.title("")
-
 centrar_texto("About me", 1, "white")
 
 centrar_texto("My name is Guillermo, I'm Argentine and I've been living in Brazil since November 2016. Married, my wife is Brazilian, her name is Samella, and so are my children. I have an almost three year old son named Benício Luca and an almost 9 year old daughter called Ester Valentina. I also have another 24-year-old son in Argentina from my first marriage, named Juan Ignacio and nickname Nacho.", 7, "white")
@@ -201,6 +192,28 @@ st.title("")
 st.markdown("<h3 style='text-align:left; color: white'>Agustin Valdes Marteles - Make it happen</h3>", unsafe_allow_html=True)
 centrar_texto("Excellent person and excellent professional. Very human and ideal for the position in which we share company. It has been a pleasure to work with him.", 7, "white")
 
+st.markdown("<h1 style='text-align: center; color: white'>Contact</h1>", unsafe_allow_html=True)
+
+with st.container():    
+    col50, col51, col52, col53, col54 = st.columns(5)
+    with col51:
+        photo_link("Kaggle", "https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/189_Kaggle_logo_logos-512.png", "https://www.kaggle.com/willycerato", "80px")
+    with col52:
+        photo_link("Github", "https://cdn.pixabay.com/photo/2022/01/30/13/33/github-6980894_1280.png", "https://github.com/Willy71", "80px")
+    with col53:
+        photo_link("Instagram", "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/150px-Instagram_logo_2022.svg.png", "https://www.instagram.com/willycerato", "80px")
+            
+st.title("")
+st.title("")
+
+with st.container():    
+    col55, col56, col57, col58, col59 = st.columns(5)
+    with col56:
+        photo_link("Facebook", "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/2023_Facebook_icon.svg/50px-2023_Facebook_icon.svg.png", "https://www.facebook.com/guillermo.cerato", "80px")
+    with col57:
+        photo_link("Linkedin", "https://img.freepik.com/vetores-premium/logotipo-quadrado-do-linkedin-isolado-no-fundo-branco_469489-892.jpg", "https://www.linkedin.com/in/willycerato", "80px")        
+    with col58:
+        photo_link("Whatsapp", "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/120px-WhatsApp.svg.png", "https://wa.me/5542991657847", "80px")
 
     
 with st.container():
