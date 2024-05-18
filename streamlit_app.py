acd0f26d52c5a882bd1de63b11304cf1afdc984c
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2024-05-18 17:23:16 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -206,7 +206,7 @@ with st.container():
     with col306:
         centrar_imagen("https://i.postimg.cc/J41hr43x/maxi-remove.png", 150)
     with col307:
-        centrar_texto_link('Maximiliano Roca Saran - Key account manager - Mobile phones', "https://www.linkedin.com/in/maximiliano-roca-saran-0b628421/", 4, 'lightblue')
+        centrar_texto_link('                                                        Maximiliano Roca Saran - Key account manager - Mobile phones', "https://www.linkedin.com/in/maximiliano-roca-saran-0b628421/", 4, 'lightblue')
 st.caption("")
 centrar_texto('Guillermo is an excellent manager, always predisposed, very focused on what he does and results-oriented.', 6, 'white')
 centrar_texto("He is a very good team builder. He is always willing to learn new things and change.", 6, "white")
