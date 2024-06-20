8ead0a34ad87b49d3330f7f9f348208e6b827a9d
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2024-06-20 20:32:52 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -303,3 +303,7 @@ with st.container():
             
 st.markdown("""<hr style="height:10px;border:none;color:#333;background-color:#333;" /> """, unsafe_allow_html=True)
 
+st.markdown("""<hr style="height:10px;border:none;color:#333;background-color:#333;" /> """, unsafe_allow_html=True)
+
+
+
