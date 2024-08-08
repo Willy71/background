f0c23e3bfa1a9049a6599ef84f15e3072de44c4e
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2024-08-08 19:24:41 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -184,7 +184,7 @@ with st.container():
 with st.container():    
     col10, col11, col12, col13, col14 = st.columns(5)
     with col10:
-        centrar_imagen_link("https://i.postimg.cc/KzV1K702/fifa2023.jpg", 'https://chicagobike.streamlit.app/', "business analyst job", 210)       
+        centrar_imagen_link("https://i.postimg.cc/KzV1K702/fifa2023.jpg", 'https://confort.streamlit.app/', "business analyst job", 210)       
     with col11:
         centrar_imagen_link("https://i.postimg.cc/wx0Wr17d/super003.jpg", 'https://github.com/Willy71/supermercados', "Supermarket", 200) 
     with col12:
