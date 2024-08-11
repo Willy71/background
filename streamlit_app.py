53df5e0256e38ec5851a7abe064a6b8f4d3c4817
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2024-08-11 10:09:36 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -116,7 +116,7 @@ st.markdown(page_bg_img, unsafe_allow_html=True)
 #st.write("#")
 st.markdown(
     f'<div style="display: flex; justify-content: center;">'
-    f'<img src="https://i.postimg.cc/MZV6b8TR/Encabezado-nombre-removebg-preview.png" width="320">'
+    f'<img src="https://github.com/Willy71/background/blob/main/picture/nombre-removebg.png?raw=true" width="320">'
     f'</div>',
     unsafe_allow_html=True
 )
